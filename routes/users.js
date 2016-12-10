f9d28a950e273d4bbdc3c8b59452d8289aa89f50
Viet <viet.le175@gmail.com>
2016-12-10 17:59:11 -0500

    test

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mraa = require('mraa');
+var myPin = new mraa.Gpio(6);
+
+var pir = require('pir').use(myPin);
+
+pir.on('ready', function(pir){
+    console.log('Ready and waiting...');
+
+      pir.on('movement:start', function(time){
+          console.log('something moved');
+      });
+      pir.on('movement:end', function(time){
+          console.log('nothing is moving now');
+      });
+});
+
+
 
 /* GET users listing. */
 router.get('/test', function(req, res, next) {
