f25820a84091f473e130859b704e581d48b8fe15
Viet <viet.le175@gmail.com>
2016-12-11 13:52:14 -0500

    fdv

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ board.on("ready", function() {
 
 
     // set up the LCD's number of columns and rows:
-    lcd.begin(16, 2);
+    lcd.cursor(16, 2);
     // Print a message to the LCD.
     lcd.print("hello, world!");
 
